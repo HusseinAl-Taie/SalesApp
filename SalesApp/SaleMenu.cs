e0f522919a3b42a92cd61208b48d6fc043e6741d
HusseinAl-Taie <haltaie@protonmail.com>
2021-10-19 13:06:51 +0100

    adding sale menu and sales class

---
+++ SalesApp/SaleMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace SalesApp
+{
+
+
+    class SaleMenu
+    {
+
+
+        enum MenuOptions
+        {
+            //using enum to represent the menu
+            CREATE, READ, DELETE, QUIT
+        }
+
+
+
+        public void PrintMenu()
+        {
+            Array values = Enum.GetValues(typeof(MenuOptions));
+
+            foreach(var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ SalesApp/dev_src/Data/Model/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SalesApp.dev_src.Data.Model
+{
+    public class Sale
+    {
+        //public Sale()
+        //{
+        //}
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Sale[ID ={ ID},  Name ={Name}]";
+            
+        }
+    }
+}
