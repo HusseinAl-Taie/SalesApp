516d94a7ef54b0679453ec7cac6cb04890bf2766
HusseinAl-Taie <haltaie@protonmail.com>
2021-10-27 18:35:12 +0100

    added the sql connection

--- SalesApp/Program.cs
+++ SalesApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using SalesApp.dev_src.Controllers;
-using SalesApp.dev_src.Data.Repositories;
-using SalesApp.dev_src.Services;
+using MySql.Data.MySqlClient;
+using SalesApp.dev_src.utils;
 
 namespace SalesApp
 {
@@ -11,13 +10,26 @@ namespace SalesApp
         {
             //creating a sale menu giving it a sale constructor as defined
 
-            SaleMenu saleMenu = new SaleMenu(
-                new SaleController(
-                    new SaleService(
-                        new SaleRepository())));
+            //SaleMenu saleMenu = new SaleMenu(
+            //    new SaleController(
+            //        new SaleService(
+            //            new SaleRepository())));
 
-            saleMenu.AppMenuLoop();
-            saleMenu.InteractiveLoop();
+            //saleMenu.AppMenuLoop();
+            //saleMenu.InteractiveLoop();
+
+            MySqlConnection connection = MySqlUtils.GetConnection();
+
+            //opening the connection
+            connection.Open();
+
+            bool connectionOpen = connection.Ping();
+
+            Console.WriteLine($@"Connection status:{ connection.State}
+Ping Succesful: { connectionOpen}");
+
+            //closing connection
+            connection.Dispose();
         }
     }
 }
